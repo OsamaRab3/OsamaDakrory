28e7b60e559c51887cc9a36c96dfaa0295f601a5
Osama Rabea <https://github.com/OsamaRab3>
2024-09-05 23:20:02 +0300

    update

--- js/main.js
+++ js/main.js
@@ -25,10 +25,6 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    let visitorCount = localStorage.getItem('visitorCount') || 0;
-    visitorCount++;
-    localStorage.setItem('visitorCount', visitorCount);
-    document.getElementById('visitor-count').textContent = visitorCount;
 });
 
  
